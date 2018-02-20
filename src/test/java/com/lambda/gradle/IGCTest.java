1b778ec1ed9c697daaa65286319f98b1dfacb0e2
Juan Esteban Echavarria <juan.echavarria@techandsolve.com>
2018-02-19 22:20:16 -0500

    Test IGC Other

--- src/test/java/com/lambda/gradle/IGCTest.java
+++ src/test/java/com/lambda/gradle/IGCTest.java
@@ -17,4 +17,11 @@ public class IGCTest {
         double igc = igcFactory.getIGC(27.68, 'm', 23);
         assertEquals(22.30,igc,0.2);
     }
+
+    @Test
+    public void getIGCOther() throws Exception{
+        IGCFactory igcFactory = new IGCFactory();
+        double igc = igcFactory.getIGC(27.68, 'o', 23);
+        assertEquals(0,igc,0.2);
+    }
 }
